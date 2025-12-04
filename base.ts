0c74427dee5e7b112770751c6dab7364478d8ce4
Andy Wilson <andy.wilson1234@gmail.com>
2025-12-04 07:06:29 +0000

    Update base.ts

--- base.ts
+++ base.ts
@@ -1,11 +1,20 @@
 import pluginPromise from 'eslint-plugin-promise';
 import pluginSecurity from 'eslint-plugin-security';
 import pluginUnicorn from 'eslint-plugin-unicorn';
+import pluginArrayFunc from 'eslint-plugin-array-func';
+import pluginSonarJS from 'eslint-plugin-sonarjs';
+import pluginNoUseExtendNative from 'eslint-plugin-no-use-extend-native';
+import { configs: pluginPutout } from 'eslint-plugin-putout';
 
 const config = [
   pluginPromise.configs['flat/recommended'],
   pluginSecurity.configs.recommended,
   pluginUnicorn.configs.all,
+  pluginArrayFunc.configs.all,
+  pluginSonarJS.configs.recommended,
+  pluginNoUseExtendNative.configs.recommended,
+  pluginPutout.recommended,
+  
 ];
 
 export default config;
